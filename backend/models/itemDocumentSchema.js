5117356eec91aa0ae749e781a4335e70f3de6048
infinity1729 <anant.bansal2003@gmail.com>
2023-04-23 11:52:09 +0530

    initial commit

---
+++ backend/models/itemDocumentSchema.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const ApprovedRequest = require('./requestSchema');
+
+const itemDocumentSchema = new mongoose.Schema({
+    "bill": String,
+    "sanctionLetter": String,
+    "inspectionReport": String,
+}, { timestamps: true });
+
+module.exports = mongoose.model("ItemDocument", itemDocumentSchema);
